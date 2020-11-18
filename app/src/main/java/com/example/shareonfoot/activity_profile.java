726b72f91d798de3dcc4719d8934cd63b632c5c1
cch230 <cch230@naver.com>
2020-11-18 23:56:51 +0900

    Internal modification
    Internal modification

--- app/src/main/java/com/example/shareonfoot/activity_profile.java
+++ app/src/main/java/com/example/shareonfoot/activity_profile.java
@@ -3,7 +3,6 @@ package com.example.shareonfoot;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
