3b424c85c653c16f4e964cd0fb4a165a8ea3fb42
cch230 <cch230@naver.com>
2020-11-18 23:58:29 +0900

    Internal modification
    Internal modification

--- app/src/main/java/com/example/shareonfoot/home/fragment_home.java
+++ app/src/main/java/com/example/shareonfoot/home/fragment_home.java
@@ -6,9 +6,7 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,27 +17,21 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
-
-import com.example.shareonfoot.Global;
-
 import com.example.shareonfoot.R;
 import com.example.shareonfoot.home.recommend.recommendPagerFragment;
 import com.example.shareonfoot.util.OnBackPressedListener;
 import com.bumptech.glide.Glide;
 import com.google.android.material.tabs.TabLayout;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-
 import retrofit2.Call;
 
 import static android.app.Activity.RESULT_OK;
