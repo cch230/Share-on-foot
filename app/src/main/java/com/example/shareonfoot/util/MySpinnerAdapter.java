14d794a0386dab09b9174efde576f77a8037bf81
cch230 <cch230@naver.com>
2020-11-18 23:56:34 +0900

    Internal modification
    Internal modification

--- app/src/main/java/com/example/shareonfoot/util/MySpinnerAdapter.java
+++ app/src/main/java/com/example/shareonfoot/util/MySpinnerAdapter.java
@@ -5,9 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-
 import java.util.List;
 
 public class MySpinnerAdapter extends ArrayAdapter<String> {
