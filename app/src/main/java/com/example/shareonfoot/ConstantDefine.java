f7c6706486de11e7f151139dfbe9723a8e867f46
cch230 <cch230@naver.com>
2020-12-09 12:20:17 +0900

    Revert merge
    Revert merge

--- app/src/main/java/com/example/shareonfoot/ConstantDefine.java
+++ app/src/main/java/com/example/shareonfoot/ConstantDefine.java
@@ -1,16 +1,8 @@
 package com.example.shareonfoot;
 
 public class ConstantDefine {
-<<<<<<< HEAD
         public static final int PERMISSION_CODE = 1;
         public static final int CAPTURE_IMAGE = PERMISSION_CODE+1;
         public static final int RESULT_OCR = CAPTURE_IMAGE+1;
 }
 
-=======
-        public static final int ACT_TAKE_PIC = 1;
-        public static final int PERMISSION_CODE = ACT_TAKE_PIC+1;
-        public static final int RESULT_OCR = PERMISSION_CODE+1;
-
-}
->>>>>>> parent of 0b5eb0b... Merge branch 'sub'
