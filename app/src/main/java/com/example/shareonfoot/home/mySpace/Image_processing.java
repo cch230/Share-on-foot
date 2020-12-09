59ec255955c4e4bcc6d62857632466cb9afe8ba0
cch230 <cch230@naver.com>
2020-12-10 03:16:11 +0900

    Improve Image processing
    Improve Image processing

--- app/src/main/java/com/example/shareonfoot/home/mySpace/Image_processing.java
+++ app/src/main/java/com/example/shareonfoot/home/mySpace/Image_processing.java
@@ -3,36 +3,58 @@ package com.example.shareonfoot.home.mySpace;
 import android.graphics.Bitmap;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Image_processing {
 
      public Bitmap ImageProcessing(Bitmap bitmap){
+         List<Rect> boundRect=new ArrayList<>();
          Mat input =new Mat();
-         Mat gray=new Mat();
-         Mat gradThresh = new Mat();
-         Mat matGaussian = new Mat();
-         Mat erode = new Mat();
-         Mat dilate = new Mat();
          Utils.bitmapToMat(bitmap,input);
-         Mat matCny = new Mat();
+         Mat img_gray =new Mat(), img_sobel=new Mat(), img_threshold=new Mat(), element=new Mat();
+         Imgproc.cvtColor(input, img_gray, Imgproc.COLOR_RGB2GRAY);
+         Imgproc.Sobel(img_gray, img_sobel, CvType.CV_8U, 1, 0, 5, 1, 0, Core.BORDER_DEFAULT);
+         //at src, Mat dst, double thresh, double maxval, int type
+         Imgproc.threshold(img_sobel, img_threshold, 0, 255, 8);
+         element=Imgproc.getStructuringElement(Imgproc.MORPH_OPEN, new Size(15,5));
+         Imgproc.morphologyEx(img_threshold, img_threshold, Imgproc.MORPH_CLOSE, element);
+         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        /* Mat hierarchy = new Mat();
+         Imgproc.findContours(img_threshold, contours,hierarchy, 0, 1);
+
+         List<MatOfPoint> contours_poly = new ArrayList<MatOfPoint>(contours.size());
+
+         for( int i = 0; i < contours.size(); i++ ){
+
+             MatOfPoint2f  mMOP2f1=new MatOfPoint2f();
+             MatOfPoint2f  mMOP2f2=new MatOfPoint2f();
+
+             contours.get(i).convertTo(mMOP2f1, CvType.CV_32FC2);
+             Imgproc.approxPolyDP(mMOP2f1, mMOP2f2, 2, true);
+             mMOP2f2.convertTo(contours.get(i), CvType.CV_32S);
+
+
+             Rect appRect = Imgproc.boundingRect(contours.get(i));
+             if (appRect.width>appRect.height) {
+                 boundRect.add(appRect);
+             }
+         }*/
          Mat result;
-         //Mat kernel= Mat.ones(9,5,CvType.CV_8U,new Scalar(1));
-         Imgproc.cvtColor(input,gray,Imgproc.COLOR_RGB2GRAY);
-         //Imgproc.adaptiveThreshold(gray, gradThresh, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 3, 12);  //block size 3
-         Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE,new Size(11,11));
-         Imgproc.dilate(gray,dilate,element);
-         Imgproc.erode(dilate,erode,new Mat());
-         Imgproc.blur(erode,matGaussian,new Size(3,3),new Point(2,2));
-         Imgproc.Canny(matGaussian,matCny,130,210,3);
-         result=removeVerticalLines(matCny,80).clone();
+         result=removeVerticalLines(img_threshold,80).clone();
          Utils.matToBitmap(result,bitmap);
          return bitmap;
      }
